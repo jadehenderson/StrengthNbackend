613976198970f6f8f463d8c425758fe1769968c8
reonrash <45687245+reonrash@users.noreply.github.com>
2021-10-05 20:28:07 -0400

    typo

--- database.sql
+++ database.sql
@@ -1,10 +1,9 @@
 
 
 CREATE TABLE USERS(
-    userID uuid PRIMARY KEY DEFAULT 
-    uuid_generate_v4(),
+    userID uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     fname VARCHAR(20) NOT NULL,
     lname VARCHAR(20) NOT NULL,
     pword VARCHAR(25) NOT NULL,
-    email VARCHAR(40) NOT NULL,
-);
+    email VARCHAR(40) NOT NULL
+)
