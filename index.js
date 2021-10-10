82ba9be3b27de78c6460a1cb01a2466358b065f5
reonrash <45687245+reonrash@users.noreply.github.com>
2021-10-09 20:16:48 -0400

    health endpoint

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(cors());
 app.use(express.json());
 const authRoute = require("./routes/jwtAuth");
 app.use("/auth", authRoute);
+app.get("/", (req, res) => {
+    res.send("We are running");
+});
 app.listen(port, async() => {
     console.log(`Server running on port:${port}`);
 })
