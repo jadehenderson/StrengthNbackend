5fa3e0d62f994a8f39d138d51480e66fd2c2240f
reonrash <45687245+reonrash@users.noreply.github.com>
2021-10-14 10:29:43 -0400

    administrator route completed

--- database.sql
+++ database.sql
@@ -12,52 +12,62 @@ drop function if exists trig;
 
 
 
+CREATE TABLE organizations(
+    organizationID SERIAL PRIMARY KEY,
+    orgname VARCHAR(255)
+);
+CREATE TABLE groups(
+    groupID SERIAL PRIMARY KEY,
+    loc VARCHAR(255),
+    startdate timestamp,
+    enddate timestamp,
+    groupname VARCHAR(100),
+    orgID INTEGER,
+    FOREIGN KEY (orgID) REFERENCES organizations(organizationID)
+);
 
-
+CREATE TABLE messages(
+    groupID INTEGER PRIMARY KEY,
+    FOREIGN KEY (groupID) REFERENCES groups(groupID)
+);
+CREATE TABLE schedules(
+    groupID INTEGER PRIMARY KEY,
+    FOREIGN KEY (groupID) REFERENCES groups(groupID)
+);
 
 CREATE TABLE users(
     userID uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     fname VARCHAR(100) NOT NULL,
     lname VARCHAR(100) NOT NULL,
     pword VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    email VARCHAR(100) NOT NULL,
+    orgID INTEGER,
+    FOREIGN KEY (orgID) REFERENCES organizations(organizationID)
 );
+
 
 CREATE TABLE userTOgroups(
-    userID uuid,
-    groupIDS int[],
-    FOREIGN KEY(userID) 
-    REFERENCES users(userID)
-
-);
-CREATE TABLE groups(
-    groupID SERIAL PRIMARY KEY,
-    userIDS uuid[],
-    name VARCHAR(100),
-    organization VARCHAR(100)
-
-);
-
-CREATE TABLE organizations(
-    organizationID SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    userIDS int[],
-    groupIDS int[]
+    userID uuid ,
+    groupID INTEGER ,
+    PRIMARY KEY(userID, groupID),
+    FOREIGN KEY (userID) REFERENCES users(userID),
+    FOREIGN KEY (groupID) REFERENCES groups(groupID)
 );
 
 
-CREATE FUNCTION trig()
-RETURNS trigger AS
+CREATE FUNCTION groupcomponents()
+RETURNS trigger AS 
 $$
-     BEGIN
-     INSERT INTO userTOgroups VALUES(NEW.userID, '{}');
-     RETURN NEW;
-     END;
+    BEGIN
+    INSERT INTO messages VALUES(NEW.groupID);
+    INSERT INTO schedules VALUES(NEW.groupID);
+    RETURN NEW;
+    END;
 $$ LANGUAGE 'plpgsql';
 
-CREATE TRIGGER boo
-  AFTER INSERT
-  ON users
-  FOR EACH ROW
-  EXECUTE PROCEDURE trig();
-
+CREATE TRIGGER onaddgroup
+    AFTER INSERT
+    ON groups
+    FOR EACH ROW
+    EXECUTE PROCEDURE groupcomponents();
+    
--- routes/administrator.js
+++ routes/administrator.js
@@ -5,13 +5,42 @@ const pool = require("../db");
 //registering
 
 router.post("/group" , async(req, res) => {
-    const {organization} = req.body;
+    const {organization, groups} = req.body;
     try {
-        const org = await pool.query("SELECT * FROM organizations WHERE name = $1 ", [organization]);
+        // create org
+        let orgid;
+        const org = await pool.query("SELECT * FROM organizations WHERE orgname = $1 ", [organization]);
         if (org.rows.length == 0) {
             console.log("here")
-            const insertOrg = await pool.query("INSERT INTO organizations(name, userids, groupids) VALUES($1, $2, $3) RETURNING *", [organization, {}, {}])
-        } 
+            const insertOrg = await pool.query("INSERT INTO organizations(orgname) VALUES($1) RETURNING *", [organization])
+            console.log(insertOrg.rows[0])
+            orgid = insertOrg.rows[0].organizationid;
+            console.log("in if")
+        } else {
+            orgid = org.rows[0].organizationid;
+        }
+        //console.log(insertOrg)
+        Object.entries(groups).forEach(async([group, emails]) => {
+            // create group
+            const newGroup = await pool.query("INSERT INTO groups(groupname, orgid) VALUES($1, $2) RETURNING *", ["meeting", orgid])
+            const {groupID} = newGroup.rows[0];
+            console.log(newGroup.rows[0]);
+            emails.forEach(async(email) => {
+                // add users 
+                // if user exists add to usertogroups table
+                const user = await pool.query("SELECT * FROM users WHERE email = $1 ", [email]);
+                if (user.rows.length == 0) {
+                    return;
+                }
+                const {userid} = user.rows[0]
+                const insertUsertoGroup = await pool.query("INSERT INTO usertogroups(userID, groupID) VALUES($1, $2) RETURNING *", [userid, groupID])
+
+
+                console.log(insertUsertoGroup)
+            }) 
+
+        })
+
     
 
         res.json(org);
@@ -24,3 +53,4 @@ router.post("/group" , async(req, res) => {
 })
 
 module.exports = router;
+
