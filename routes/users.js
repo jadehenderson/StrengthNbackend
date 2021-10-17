a9f50cf283a55c8fbe32828bd478da79cf9b838c
reonrash <45687245+reonrash@users.noreply.github.com>
2021-10-16 21:08:38 -0400

    had duplicate lines of import

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const pool = require("../db");
 
-const router = require("express").Router();
-const pool = require("../db");
 const authorization = require("../middleware/authorization");
 
 
