1310c15dd8731c4e4c58b8d8197b651b0e1061a3
Leon <reonrash@gmail.com>
2021-11-22 16:27:55 -0500

    schedule

--- routes/administrator.js
+++ routes/administrator.js
@@ -56,50 +56,6 @@ router.post("/group", async (req, res) => {
 				[numMembers, groupid]
 			);
 		}
-
-		/*
-		Object.entries(groups).forEach(async ([group, emails]) => {
-			// create group
-			const newGroup = await pool.query(
-				"INSERT INTO groups(groupname, orgid) VALUES($1, $2) RETURNING *",
-				["meeting", orgid]
-			);
-			let numMembers = 0;
-			const { groupid } = newGroup.rows[0];
-
-			emails.forEach(async (email) => {
-				// add users
-				// if user exists add to usertogroups table
-				const user = await pool.query("SELECT * FROM users WHERE email = $1 ", [
-					email,
-				]);
-
-				if (user.rows.length == 0) {
-					return;
-				}
-				numMembers += 1;
-				const { userid, fname } = user.rows[0];
-				const updateUserOrg = await pool.query(
-					"UPDATE users SET orgID = $1 WHERE userID = $2",
-					[orgid, userid]
-				);
-
-				const insertUsertoGroup = await pool.query(
-					"INSERT INTO usertogroups(userID, groupID) VALUES($1, $2) RETURNING *",
-					[userid, groupid]
-				);
-			});
-			await sleep(20000);
-			const updateGroupCount = await pool.query(
-				"UPDATE schedules SET nummembers = $1 WHERE groupID = $2 RETURNING *",
-				[numMembers, groupid]
-			);
-			//console.log(groupid);
-			console.log(updateGroupCount.rows[0]);
-
-			console.log(numMembers, updateGroupCount.rows[0].nummembers);
-		});
-		*/
 		res.status(201).json({ msg: "Successfully made groups" });
 	} catch (err) {
 		console.log(err);
